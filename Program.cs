6834f5d8c792d1f71503191e1ee6ca6487c4f9e4
Marko-Pajic <pajic.marko84@gmail.com>
2023-06-12 12:55:34 +0200

    Fixed #3, resolved #6, changed the condition of while loop and combined boolean with for loop

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ namespace ConsoleApp4
     {
         public static readonly Random rnd = new Random();
         public const int GUESS_LIMIT = 6;
+
         static void Main(string[] args)
         {
             //introduction to the program
@@ -13,6 +14,7 @@ namespace ConsoleApp4
             Console.WriteLine("You will be guessing letters inside of the secret word...");
             Console.WriteLine($"You have the right to {Program.GUESS_LIMIT} wrong guesses!");
             Console.WriteLine("Lets go!");
+
             //creating a list with strings that are been used as guessing words
             List<string> words = new List<string>() { "mayhem", "chaos", "destruction" };
             //block of code that is randomly picking string out of the list
@@ -26,25 +28,28 @@ namespace ConsoleApp4
                 Console.Write(guessedWord[i]);
             }
             Console.WriteLine();
+
             int numOfWrongGuesses = 0;
-            bool guessedRight = false;
+            bool guessedRight = true;
             //usign while loop to loop through conditions until one occurs
-            while (true)
+            while (guessedRight)
             {
                 //asking for input and checking if entered input exists in randomly picked word which we store in a boolean
                 char guessingLetter = Console.ReadKey().KeyChar;
                 Console.WriteLine();
-                bool charExists = randomWord.Contains(guessingLetter);
+                bool charExists = false;
                 //using the condition of boolean to procced with building our word or...*
+                for (int i = 0; i < randomWord.Length; i++)
+                {
+                    if (randomWord[i] == guessingLetter)
+                    {
+                        charExists = true;
+                        guessedWord[i] = guessingLetter;
+                    }
+                }
+
                 if (charExists)
                 {
-                    for (int i = 0; i < randomWord.Length; i++)
-                    {
-                        if (randomWord[i] == guessingLetter)
-                        {
-                            guessedWord[i] = guessingLetter;
-                        }
-                    }
                     Console.WriteLine(guessedWord);
                     //buiding an string out of char array to be able to check if player guessed the randomly picked word
                     string guessedWordString = new string(guessedWord);
@@ -63,19 +68,21 @@ namespace ConsoleApp4
                     //checking if player player exceeded the limit of wrong guesses
                     if (numOfWrongGuesses >= Program.GUESS_LIMIT)
                     {
+                        guessedRight = false;
                         break;
                     }
                     Console.WriteLine($"You have {Program.GUESS_LIMIT - numOfWrongGuesses} shots left.");
                     Console.WriteLine("Try again...\n");
                 }
             }
-            if (guessedRight)
+
+            if (!guessedRight)
             {
-                Console.WriteLine("You won the game!");
+                Console.WriteLine("You lost the game!");
             }
             else
             {
-                Console.WriteLine("You lost the game!");
+                Console.WriteLine("You won the game!");
             }
         }
     }
